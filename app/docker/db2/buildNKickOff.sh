62f050993f49086d2374e6aecc7cddcc260242a1
root <root@DESKTOP-G9MU7F2.localdomain>
2020-10-18 18:04:30 -0500

    Database container updates

--- app/docker/db2/buildNKickOff.sh
+++ app/docker/db2/buildNKickOff.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/bash
 
-DOCKER_NAME=edts-flask
+DOCKER_NAME=edts-db
 VERSION=1.1
 
 if [ -e back-end ]; then
-  rm -rf back-end
+  rm -rf default.sql
 fi
-cp -r ../../back-end .
+cp -r ../../db/default.sql .
 
 docker stop $DOCKER_NAME
 docker rm $DOCKER_NAME
 
 docker build -t $DOCKER_NAME:$VERSION .
-docker create --name $DOCKER_NAME -P -t -p 5000:5000 $DOCKER_NAME:$VERSION
+docker create --name $DOCKER_NAME -P -t $DOCKER_NAME:$VERSION
 docker start $DOCKER_NAME

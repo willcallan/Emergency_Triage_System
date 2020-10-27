d7abb31832978ebaf1d378f1aebaac4c5f1b210f
jcampbell308@gatech.edu <jcampbell308@gatech.edu>
2020-10-26 22:47:36 -0500

    Add new submodule, hello FHIR example, swagger, and requirements

--- app/back-end/hello.py
+++ app/back-end/hello.py
@@ -1,31 +1,21 @@
-from flask import Flask
+from flask import Blueprint
 from fhirclient import client
 import fhirclient.models.patient as pat
-import socket
+from vars import settings
+from flasgger.utils import swag_from
 
-app = Flask(__name__)
-
-hostname = socket.gethostname()
-IPAddr = socket.gethostbyname(hostname)
-app.config['SERVER_NAME']= IPAddr + ":5000"
-
-settings = {
-    'app_id': 'NewHat_Triage_Program',  # TODO: Come up with name?
-    'api_base': 'https://r4.smarthealthit.org'
-}
+hello_world = Blueprint('hello_world',__name__)
 
 
-@app.route("/fhir")
+@hello_world.route("/fhir")
+@swag_from('static/hello_world_fhir.yml')
 def hello_fhir():
     smart = client.FHIRClient(settings=settings)
     patient = pat.Patient.read('d0190651-b9b0-4513-8f3b-d542319220d1',smart.server)
     return smart.human_name(patient.name[0])
 
 
-@app.route("/")
+@hello_world.route("/")
+@swag_from('static/hello_world.yml')
 def hello():
     return "Hello World!"
-
-
-if __name__ == "__main__":
-    app.run()

37b12a8052f0866ac85fb94e2dcfcab873e2328a
jcampbell308@gatech.edu <jcampbell308@gatech.edu>
2020-10-26 21:21:01 -0500

    Add new submodule, hello FHIR example, and requirements

--- app/back-end/hello.py
+++ app/back-end/hello.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from fhirclient import client
+import fhirclient.models.patient as pat
 import socket
 
 app = Flask(__name__)
@@ -6,6 +8,19 @@ app = Flask(__name__)
 hostname = socket.gethostname()
 IPAddr = socket.gethostbyname(hostname)
 app.config['SERVER_NAME']= IPAddr + ":5000"
+
+settings = {
+    'app_id': 'NewHat_Triage_Program',  # TODO: Come up with name?
+    'api_base': 'https://r4.smarthealthit.org'
+}
+
+
+@app.route("/fhir")
+def hello_fhir():
+    smart = client.FHIRClient(settings=settings)
+    patient = pat.Patient.read('d0190651-b9b0-4513-8f3b-d542319220d1',smart.server)
+    return smart.human_name(patient.name[0])
+
 
 @app.route("/")
 def hello():

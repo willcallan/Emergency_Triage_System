9c1243a1329db0529ce8709e1c2a527a5f278910
Menon, Vidya <vmenon48@gatech.edu>
2020-11-01 15:27:53 -0600

    updated the script

--- app/db/TriageDB.sql
+++ app/db/TriageDB.sql
@@ -1,62 +1,32 @@
--- Database: TriageDB
-
-
-
--- DROP DATABASE "TriageDB";
-
-
-
-CREATE DATABASE "TriageDB"
-
-    WITH
-
-    OWNER = postgres
-
-    ENCODING = 'UTF8'
-
-    LC_COLLATE = 'English_United States.1252'
-
-    LC_CTYPE = 'English_United States.1252'
-
-    TABLESPACE = pg_default
-
-    CONNECTION LIMIT = -1;
-
-
-
-
-
-CREATE TABLE public."Tbl_TriagePatient"
+CREATE TABLE "Tbl_TriagePatient"
 
 (
 
     "TriagePatientId" bigint NOT NULL,
 
-    "FHIRPatientId" bigint,
+    "FHIRPatientId" text,
 
     PRIMARY KEY ("TriagePatientId")
 
 );
 
+CREATE TABLE "Tbl_TriageProfessional"
 
+(
 
-ALTER TABLE public."Tbl_TriagePatient"
+    "TriageProfessionalId" bigint NOT NULL,
 
-    OWNER to postgres;
+    "FHIRPractionerId" text,
 
+    "TriageWorkStatusId" bigint,
 
+    "ProfessionalType" text COLLATE pg_catalog."default",
 
+    CONSTRAINT "Tbl_TriageProfessional_pkey" PRIMARY KEY ("TriageProfessionalId")
 
+);
 
--- Table: public.Tbl_TriagePatientDetail
-
-
-
--- DROP TABLE public."Tbl_TriagePatientDetail";
-
-
-
-CREATE TABLE public."Tbl_TriagePatientDetail"
+CREATE TABLE "Tbl_TriagePatientDetail"
 
 (
 
@@ -76,7 +46,7 @@ CREATE TABLE public."Tbl_TriagePatientDetail"
 
     CONSTRAINT "TblTriagePatient_TblTriagePatientDetail" FOREIGN KEY ("TriagePatientId")
 
-        REFERENCES public."Tbl_TriagePatient" ("TriagePatientId") MATCH SIMPLE
+        REFERENCES "Tbl_TriagePatient" ("TriagePatientId") MATCH SIMPLE
 
         ON UPDATE NO ACTION
 
@@ -84,7 +54,7 @@ CREATE TABLE public."Tbl_TriagePatientDetail"
 
     CONSTRAINT "Tbl_TriagePatientDetail_TriagePractionerId_fkey" FOREIGN KEY ("TriagePractionerId")
 
-        REFERENCES public."Tbl_TriageProfessional" ("TriageProfessionalId") MATCH SIMPLE
+        REFERENCES "Tbl_TriageProfessional" ("TriageProfessionalId") MATCH SIMPLE
 
         ON UPDATE NO ACTION
 
@@ -92,35 +62,9 @@ CREATE TABLE public."Tbl_TriagePatientDetail"
 
 
 
-)
+);
 
-
-
-    TABLESPACE pg_default;
-
-
-
-ALTER TABLE public."Tbl_TriagePatientDetail"
-
-    OWNER to postgres;
-
--- Index: ProfessionalId
-
-
-
--- DROP INDEX public."ProfessionalId";
-
-
-
-CREATE INDEX "ProfessionalId"
-
-    ON public."Tbl_TriagePatientDetail" USING btree
-
-        ("TriagePractionerId" ASC NULLS LAST)
-
-    TABLESPACE pg_default;
-
-CREATE TABLE public."Tbl_TriagePatientStatus"
+CREATE TABLE "Tbl_TriagePatientStatus"
 
 (
 
@@ -136,7 +80,7 @@ CREATE TABLE public."Tbl_TriagePatientStatus"
 
     FOREIGN KEY ("TriagePatientDetailId")
 
-        REFERENCES public."Tbl_TriagePatientDetail" ("TriagePatientDetailId") MATCH SIMPLE
+        REFERENCES "Tbl_TriagePatientDetail" ("TriagePatientDetailId") MATCH SIMPLE
 
         ON UPDATE NO ACTION
 
@@ -146,23 +90,7 @@ CREATE TABLE public."Tbl_TriagePatientStatus"
 
 );
 
-
-
-ALTER TABLE public."Tbl_TriagePatientStatus"
-
-    OWNER to postgres;
-
-
-
--- Table: public.Tbl_TriageESIStatus
-
-
-
--- DROP TABLE public."Tbl_TriageESIStatus";
-
-
-
-CREATE TABLE public."Tbl_TriageESIStatus"
+CREATE TABLE "Tbl_TriageESIStatus"
 
 (
 
@@ -178,65 +106,9 @@ CREATE TABLE public."Tbl_TriageESIStatus"
 
     CONSTRAINT "Tbl_TriageESIStatus_pkey" PRIMARY KEY ("TriageESIStatusId")
 
-)
+);
 
-
-
-    TABLESPACE pg_default;
-
-
-
-ALTER TABLE public."Tbl_TriageESIStatus"
-
-    OWNER to postgres;
-
-
-
--- Table: public.Tbl_TriageProfessional
-
-
-
--- DROP TABLE public."Tbl_TriageProfessional";
-
-
-
-CREATE TABLE public."Tbl_TriageProfessional"
-
-(
-
-    "TriageProfessionalId" bigint NOT NULL,
-
-    "FHIRPractionerId" bigint,
-
-    "TriageWorkStatusId" bigint,
-
-    "ProfessionalType" text COLLATE pg_catalog."default",
-
-    CONSTRAINT "Tbl_TriageProfessional_pkey" PRIMARY KEY ("TriageProfessionalId")
-
-)
-
-
-
-    TABLESPACE pg_default;
-
-
-
-ALTER TABLE public."Tbl_TriageProfessional"
-
-    OWNER to postgres;
-
-
-
--- Table: public.TriageErrorLog
-
-
-
--- DROP TABLE public."TTbl_riageErrorLog";
-
-
-
-CREATE TABLE public."Tbl_TriageErrorLog"
+CREATE TABLE "Tbl_TriageErrorLog"
 
 (
 
@@ -246,29 +118,9 @@ CREATE TABLE public."Tbl_TriageErrorLog"
 
     "TimeStampCreated" date
 
-)
+);
 
-
-
-    TABLESPACE pg_default;
-
-
-
-ALTER TABLE public."Tbl_TriageErrorLog"
-
-    OWNER to postgres;
-
-
-
--- Table: public.Tbl_TriageWorkStatus
-
-
-
--- DROP TABLE public."Tbl_TriageWorkStatus";
-
-
-
-CREATE TABLE public."Tbl_TriageWorkStatus"
+CREATE TABLE "Tbl_TriageWorkStatus"
 
 (
 
@@ -282,14 +134,24 @@ CREATE TABLE public."Tbl_TriageWorkStatus"
 
     CONSTRAINT "TriageWorkStatus_pkey" PRIMARY KEY ("TriageWorkStatusId")
 
-)
+);
+
+INSERT INTO "Tbl_TriagePatient" ("FHIRPatientId","TriagePatientId") values ('fc200fa2-12c9-4276-ba4a-e0601d424e55',1);
+INSERT INTO "Tbl_TriagePatient" ("FHIRPatientId","TriagePatientId") values ('689892bd-dcbe-41fc-8651-38a1d0893854',2);
+
+INSERT INTO "Tbl_TriageProfessional" ("TriageProfessionalId", "FHIRPractionerId", "TriageWorkStatusId", "ProfessionalType")
+ VALUES (1, 'efb5d4ce-dffc-47df-aa6d-05d372fdb407',NULL,'Doctor');
+INSERT INTO "Tbl_TriageProfessional" ("TriageProfessionalId", "FHIRPractionerId", "TriageWorkStatusId", "ProfessionalType")
+VALUES (1, '5e57a286-d7c6-4e2d-9834-7fb48bd32b51',NULL,'Doctor');
+
+INSERT INTO "Tbl_TriagePatientDetail" ("TriagePatientDetailId", "TriagePatientId", "TriagePractionerId", "FirstEncounterDate", "DischargeDate", "Active") VALUES
+(1,1,1,'2020-10-28','2020-11-01',1);
 
 
 
-    TABLESPACE pg_default;
 
 
 
-ALTER TABLE public."Tbl_TriageWorkStatus"
 
-    OWNER to postgres;
+
+

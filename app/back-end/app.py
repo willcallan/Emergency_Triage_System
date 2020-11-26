869f428ac131aec145b6c3638c6cd16c2e8b22cf
jcampbell308@gatech.edu <jcampbell308@gatech.edu>
2020-11-26 07:50:52 -0600

    Reset port to 5000

--- app/back-end/app.py
+++ app/back-end/app.py
@@ -23,7 +23,7 @@ app.register_blueprint(observation_endpoint)
 
 hostname = socket.gethostname()
 IPAddr = socket.gethostbyname(hostname)
-app.config['SERVER_NAME']= IPAddr + ":500"
+app.config['SERVER_NAME']= IPAddr + ":5000"
 
 swagger = Swagger(app)
 
